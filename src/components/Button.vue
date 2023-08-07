48b22de7e14ae47081bda1b180c01a05a1b03116
Lucas Silva <ssilvalucas2004@gmail.com>
2023-08-07 15:27:54 -0300

    fix: fix responsive in xs devices

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -11,7 +11,7 @@ const { showModal } = storeToRefs(store)
 
 <template>
   <Share v-if="showModal"/>
-  <div class="flex w-screen justify-center">
-    <button class="border border-solid rounded-md bg-ctp-base text-ctp- font-Prompt border-ctp-text p-3 w-full md:w-3/5 max-w-xs" @click="showModal = true">Share</button>
+  <div class="flex w-full px-4 mb-2 justify-center">
+    <button class="border border-solid rounded-md bg-ctp-base font-Prompt border-ctp-text p-3 w-72 md:w-full max-w-xs" @click="showModal = true">Share</button>
   </div>
 </template>
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -19,8 +19,8 @@ const defineUser = () => {
 
     const promisse = fetch(`https://api.github.com/users/${state.user}`);
 
-    promisse
-      .then((res) => {
+    promisse.
+      then((res) => {
         if(!res.ok) {
         if(res.status === 404) {
           window.alert("user not finded")
@@ -48,11 +48,11 @@ const defineUser = () => {
 
 <template>
   <div class="flex justify-center items-center bg-ctp-mantle flex-col h-screen md:w-screen pb-6 text-ctp-lavender">
-    <h1 class="text-4xl md:text-6xl p-3 text-ctp-lavender font-Prompt">Find Your Github Card</h1>
-    <div class="flex flex-col justify-center items-center p-3 rounded-md border-ctp-text border bg-ctp-base md:max-w-xs md:w-3/5 m-2">
-      <div class="flex justify-center">
-        <input class="p-1 border border-solid border-ctp-text rounded-l-md w-50 outline-none appearance-none bg-transparent" placeholder="search a username..." type="search" v-model="state.searchInput" @keydown.enter="defineUser"/>
-        <button class="border border-ctp-text border-l-0 p-1 rounded-r-md  w-full" @click="defineUser" >search</button>
+    <h1 class="text-4xl md:text-6xl md:p-3 text-ctp-lavender font-Prompt">Find Your Github Card</h1>
+    <div class="flex flex-col justify-center items-center p-3 rounded-md border-ctp-text border bg-ctp-base max-w-xs md:w-3/5 m-2">
+      <div class="px-0 flex justify-center w-full">
+        <input class="p-1 border border-solid border-ctp-text rounded-l-md outline-none appearance-none bg-transparent" placeholder="search a username..." type="search" v-model="state.searchInput" @keydown.enter="defineUser"/>
+        <button class="border border-ctp-text border-l-0 p-1 rounded-r-md" @click="defineUser" >search</button>
       </div>
       <img class="rounded-full w-56 h-56 md:w-64 md:h-64 m-4" :src="state.pic" alt="foto de perfil do github" />
       <h2 class="text-2xl md:text-3xl font-Prompt">@{{ state.login }}</h2>

6bd2c0d3db438674f82eb11f3026b1ba36d28e94
Lucas Silva <ssilvalucas2004@gmail.com>
2023-10-06 18:00:28 -0300

    feat: adding and configuring vuerouter to project

---
+++ src/router/index.js
@@ -0,0 +1,34 @@
+import { createWebHistory, createRouter } from "vue-router";
+import Home from "../pages/Index.vue";
+import CardPage from "../pages/CardPage.vue";
+import Repos from "../pages/Repos.vue";
+import NotFound from "../pages/404Page.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/card",
+    name: "card",
+    component: CardPage,
+  },
+  {
+    path: "/repos",
+    name: "repos",
+    component: Repos,
+  },
+  {
+    path: "/:catchAll(.*)",
+    component: NotFound,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;

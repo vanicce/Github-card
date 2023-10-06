8cda19227d29f2908e2d5ca92dc99bdee4320436
Lucas Silva <ssilvalucas2004@gmail.com>
2023-10-06 18:01:45 -0300

    feat: creating pages to routing

---
+++ src/pages/Repos.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1 class="text-3xl md:text-5xl 2xl:text-6xl p-3 text-ctp-lavender font-Prompt">In Production...</h1>
+</template>
--- src/pages/cardPage.vue
+++ src/pages/cardPage.vue
@@ -1,9 +1,12 @@
 <script setup>
 // import Wrapper from '../components/wrapper.vue';
-import Card from '../components/Card.vue';
+import { defineAsyncComponent } from 'vue';
+
+const Card = defineAsyncComponent(() =>
+  import('../components/Card.vue')
+)
 </script>
 
 <template>
-  <h1 class="text-3xl md:text-5xl 2xl:text-6xl p-3 text-ctp-lavender font-Prompt">Find Your GitHub Card</h1>
   <Card />
 </template>

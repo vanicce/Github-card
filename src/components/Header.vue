fe22ed672d8c66fa548d61d0619117b8a33f3f7f
Lucas Silva <ssilvalucas2004@gmail.com>
2023-11-10 17:16:42 -0300

    feat: creating custom URL's to cards and improving way the router works

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,23 +1,32 @@
 <script setup>
 import { useCardStore } from '../stores/useCardStore';
+import router from '../router/index'
 
 const store = useCardStore()
 
+document.addEventListener('keydown', (event) => {
+  if (event.ctrlKey) {
+    if (event.key === 'i') {
+      document.querySelector('input').focus()
+      event.preventDefault()
+    }
+  }
+})
 
 const defineUser = async () => {
+  const routerPath = router.currentRoute.value.params.user
   document.querySelector('input').blur()
 
-  if (store.searchInput === "") {
+  if (store.searchInput === "" && routerPath === 'your name') {
     window.alert("write a username");
     return
   }
-
-  const response = await fetch(`https://api.github.com/users/${store.searchInput}`);
+  console.log('oiii');
+  const response = await fetch(`https://api.github.com/users/${store.searchInput || routerPath}`);
   try {
     if (!response.ok) {
       throw Error()
     }
-
 
     const userData = await response.json()
     const { bio, avatar_url, login, followers, following, public_repos, blog, html_url } = userData
@@ -32,16 +41,28 @@ const defineUser = async () => {
 
     store.url = html_url
 
-    store.searchInput = ''
   } catch (error) {
     response.status === 404 ? (
       alert("user not finded")
     ) : (
       alert(`an error in requisiton ocurred: ${response.status} ${response.statusText}`)
     )
+  } finally {
+    router.push({ name: 'card', params: { user: store.login } })
+    store.searchInput = ''
   }
 };
 
+setTimeout(() => {
+  const routerPath = router.currentRoute.value.params.user
+
+  const url = (() => {
+    if (routerPath != "your name" && routerPath != undefined) {
+      defineUser()
+    }
+  })()
+
+}, 1000)
 </script>
 
 <template>
@@ -50,8 +71,8 @@ const defineUser = async () => {
     <input
       class="p-1 border border-solid border-ctp-text rounded-l-md outline-none appearance-none bg-transparent w-full "
       placeholder="search a username..." type="search" v-model="store.searchInput" @keydown.enter="defineUser" />
-    <button class="border border-ctp-text border-l-0 p-1 rounded-r-md" @click="defineUser">search</button>
+    <button id="searchBtn" class="border border-ctp-text border-l-0 p-1 rounded-r-md" @click="defineUser">search</button>
     <router-link to="/repos" class="underline px-4">repos</router-link>
-    <router-link to="/card" class="underline px-4">card</router-link>
+    <router-link id="card" :to="{ params: { user: store.login }, name: 'card' }" class="underline px-4">card</router-link>
   </div>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -11,7 +11,7 @@ const routes = [
     component: Home,
   },
   {
-    path: "/card",
+    path: "/card/:user",
     name: "card",
     component: CardPage,
   },

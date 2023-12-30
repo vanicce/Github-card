465a5e23e9d7e4496672b16289a7598fa8a02bb1
Lucas <ssilvalucas2004@gmail.com>
2023-12-30 13:34:21 -0300

    refactor: refactoring entire share component

--- src/components/Share.vue
+++ src/components/Share.vue
@@ -24,23 +24,17 @@ const copy = async () => {
 
 <template>
   <div class="flex h-screen w-screen justify-center items-center fixed left-0 top-0">
-    <div class="flex justify-between flex-col p-11 m-2 w-full sm:w-2/4 border rounded-md">
-      <div class="flex justify-end">
+    <div class="flex justify-between flex-col px-3 p-7 m-2 w-full sm:w-2/3 md:w-auto border rounded-lg bg-white">
+      <div class="flex items-start justify-end">
         <button @click="showModal = false">
-          <img class="fill-white" src="../assets/x-symbol.svg" width="25" height="25">
+          <img src="../assets/x-symbol.svg" width="25" height="25">
         </button>
       </div>
       <div class="flex flex-col md:flex-row md:justify-evenly justify-center items-center">
-        <h1 class="text-xl ">share:</h1>
-        <p
-          class="flex items-center justify-between border border-solid rounded-md border-white w-full sm:w-3/4 p-2 m-2 font-Prompt">
-          {{
-            state.url }}
-          <button class="border border-white px-4 py-2 rounded-md text-black" @click="copy()">
-            copy
-            <!-- <img src="../assets/copy-svgrepo-com (1).svg" width="25" height="25"> -->
-          </button>
+        <h1 class="text-lg">share:</h1>
+        <p class="flex items-center justify-between flex-col border border-solid rounded-md border-zinc-300 w-full sm:min-w-3/4 p-2 m-2 font-Prompt"> {{ state.url }}
         </p>
+        <button class="border border-zinc-300 px-4 py-2 rounded-md text-black" @click="copy()">copy</button>
       </div>
       <Transition>
         <div v-if="store.copied" class="flex items-center justify-center">

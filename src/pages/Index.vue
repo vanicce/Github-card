74494f0784c74309b7f3b14c0ddfb2de744fad4c
Lucas <ssilvalucas2004@gmail.com>
2023-12-30 14:25:30 -0300

    feat: centering text in index component

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -5,6 +5,6 @@
 </script>
 
 <template>
-  <h1 class="text-3xl md:text-5xl 2xl:text-6xl p-3 font-Prompt">Find Your GitHub Card</h1>
-  <router-link :to="{ params: { user: store.login}, name: 'card'}" class="px-4 py-3 rounded-md font-Prompt font-bold">Start</router-link>
+  <h1 class="text-3xl md:text-5xl 2xl:text-6xl p-3 font-Prompt text-center">Find Your GitHub Card</h1>
+  <router-link :to="{ params: { user: store.login}, name: 'card'}" class="px-5 py-3 rounded-lg font-Prompt font-bold border border-zinc-300 hover:scale-110 transition-transform">Start</router-link>
 </template>

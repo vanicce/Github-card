4e6d5f892738492fbcc0d799a971e2cea33f00f1
Lucas <ssilvalucas2004@gmail.com>
2023-12-30 14:33:51 -0300

    fix: fixing responsive design of footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,7 +4,7 @@
 
 <template>
   <div
-    class="fixed bottom-0 flex justify-center items-center w-full h-20 border bg-zinc-50">
+    class=" md:fixed md:bottom flex justify-center items-center w-full h-20 border bg-zinc-50">
     <h2>made with ❤ by <a class="underline" href="https://github.com/lucwx" target="_blank">Lucas</a>.</h2>
   </div>
 </template>

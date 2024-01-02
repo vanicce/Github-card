952ce35fdd6778c7c1461dbf82706c71867056d0
Lucas Silva <ssilvalucas2004@gmail.com>
2024-01-02 16:11:24 -0300

    feat: improving responsive design in mobile

--- src/components/Wrapper.vue
+++ src/components/Wrapper.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <div class="flex flex-col justify-center items-center bg-zinc-50 min-h-[100dvh] h-full py-4">
+  <div class="flex flex-col justify-start md:justify-center items-center bg-zinc-50 md:min-h-[100dvh] h-full py-4">
     <slot></slot>
   </div>
 </template>

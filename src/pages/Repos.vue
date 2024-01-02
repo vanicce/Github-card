4c747109bcf7217c220536fad52e1e3ff29e1425
Lucas Silva <ssilvalucas2004@gmail.com>
2024-01-02 16:16:59 -0300

    fix: fixing responsive problems

--- src/pages/Repos.vue
+++ src/pages/Repos.vue
@@ -1,3 +1,5 @@
 <template>
-  <h1 class="text-3xl md:text-5xl 2xl:text-6xl p-3 font-Prompt">In Production...</h1>
+  <div class="h-[100dvh]">
+    <h1 class="text-3xl md:text-5xl 2xl:text-6xl p-3 font-Prompt">In Production...</h1>
+  </div>
 </template>

0119d8f18092fdf994a745e2f9feb6624bb84ed1
Lucas Silva <ssilvalucas2004@gmail.com>
2024-01-02 15:56:22 -0300

    fix: fixing responsive problem

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,7 +4,7 @@
 
 <template>
   <div
-    class=" md:fixed md:bottom-0 flex justify-center items-center w-full h-20 border bg-zinc-50">
+    class="flex justify-center items-center w-full h-20 border bg-zinc-50">
     <h2>made with ❤ by <a class="underline" href="https://github.com/lucwx" target="_blank">Lucas</a>.</h2>
   </div>
 </template>
